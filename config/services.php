5c49c06a922324f8fcc1c1c430c9f0e0d5565d19
thavoo <gustavoh.2312@gmail.com>
2018-06-01 08:47:27 -0400

    Se ajustaron los procesos.

--- config/services.php
+++ config/services.php
@@ -37,8 +37,8 @@ return [
 
 
     'facebook' => [
-    'client_id' => env ('FACEBOOK_ID'),
-    'client_secret' => env ('FACEBOOK_SECRET'),
-    'redirect' => env ('FACEBOOK_URL') ,
+    'client_id' => env('FACEBOOK_ID'),
+    'client_secret' => env('FACEBOOK_SECRET'),
+    'redirect' => env('FACEBOOK_URL'),
     ],
 ];
